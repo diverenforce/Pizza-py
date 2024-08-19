1f6a7b121fe7476f7937c589ec0623554ea09210
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-19 12:50:36 +0630

    There is 2 plants at least on world

--- pizza.py
+++ pizza.py
@@ -1,14 +1,27 @@
 import sys
 
 def main():
-    ...
+    if arg_length() and len(sys.argv) < 2:
+        sys.exit('Too few command-line arguments')
+    elif arg_length() and len(sys.argv) > 2:
+        sys.exit('Too many command-line arguments')
+    
+    if arg_end() == False:
+        sys.exit('Not a CSV file')
+
+    if arg_exist() != True:
+        sys.exit('File does not exit')
 
 def arg_length():
-    return len(sys.argv) == 2
+    return len(sys.argv) != 2
 
 def arg_end():
     return sys.argv[1].endswith('.csv')
 
+def arg_exist():
+    open(sys.argv[1])
+    
 
 
-
+if __name__ == '__main__':
+    main()
